f271ec7526f1a8fd1684f525a444e4947e9f0edf
Dan Gowans <dangowans@users.noreply.github.com>
2025-01-08 11:11:41 -0500

    jsdoc

--- types.ts
+++ types.ts
@@ -12,15 +12,37 @@ export type NtfyMessagePriority =
   | 'min'
 
 export interface NtfyMessageOptions {
+  /** The ntfy server to publish to */
   server?: string
+
+  /** The notification topic */
   topic: string
+
+  /** The notification priority */
   priority?: NtfyMessagePriority
+
+  /** The notification title */
   title?: string
+
+  /** The notification message body */
   message?: string
+
+  /**
+   * The notification tags
+   * - If the tag matches a supported emoji short code, the emoji will be prepended to the title or message.
+   * - If the tag does not match, the tag will be listed below the notification.
+   */
   tags?: string[]
+
+  /** Whether to cache the notification. */
   cache?: boolean
+
+  /** The URL to open when the notification is clicked. */
   clickURL?: string
+
+  /** The URL to the icon to appear next to the notification. */
   iconURL?: string
+
   fileAttachmentURL?: string
   fileName?: string
 }
