16dc77cff8356e9fd774695a9feb0a313cb9de10
Dan Gowans <dangowans@users.noreply.github.com>
2025-01-08 11:11:16 -0500

    allow ntfy as a word

--- eslint.config.d.ts
+++ eslint.config.d.ts
@@ -1,1 +1,2 @@
-export { default } from 'eslint-config-cityssm';
+export declare const config: import("@typescript-eslint/utils/dist/ts-eslint").FlatConfig.ConfigArray;
+export default config;
--- eslint.config.ts
+++ eslint.config.ts
@@ -1,1 +1,19 @@
-export { default } from 'eslint-config-cityssm'
+import { configWebApp, cspellWords, tseslint } from 'eslint-config-cityssm'
+
+export const config = tseslint.config(...configWebApp, {
+  rules: {
+    '@cspell/spellchecker': [
+      'warn',
+      {
+        cspell: {
+          words: [
+            ...cspellWords,
+            'ntfy'
+          ]
+        }
+      }
+    ]
+  }
+})
+
+export default config
